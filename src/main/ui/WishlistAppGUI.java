e0d81a8946478f9ca4c1aa6869539b9ea8fdba82
drkchu <derekdbchu@gmail.com>
2023-03-19 18:54:44 -0700

    Phase 3: GUI
    Working on task 3:
    - Added addItemButton
    - Need to work on displaying the items in the wishlist!

--- src/main/ui/WishlistAppGUI.java
+++ src/main/ui/WishlistAppGUI.java
@@ -1,25 +1,124 @@
 package ui;
 
+import model.Item;
 import model.Wishlist;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class WishlistAppGUI extends JFrame {
+public class WishlistAppGUI extends JFrame implements ActionListener {
+    public static final int WIDTH = 1080;
+    public static final int HEIGHT = 720;
+    public static final Color BACKGROUND_COLOR = new Color(35,47,62);
+    public static final Color TEXT_COLOR = new Color(255,153,0);
+
     private static final String JSON_STORE = "./data/wishlist.json";
+    private static final String ICON_STORE = "./data/wishlistManagerIcon.png";
 
     private JsonReader jsonReader;
     private JsonWriter jsonWriter;
     private Wishlist wishlist;
     private Scanner scan;
 
+    private JButton addItemButton;
     public WishlistAppGUI() {
-        public static final int WIDTH = 1080;
-        public static final int HEIGHT = 720;
-
+        super("Wishlist Manager");
+        initializeWishlist();
+        initializeFrame();
+        initializeButtons();
         jsonReader = new JsonReader(JSON_STORE);
         jsonWriter = new JsonWriter(JSON_STORE);
+        this.setVisible(true);
+        //this.pack();
+    }
+
+    private void initializeButtons() {
+        addItemButton = new JButton();
+        addItemButton.setSize(new Dimension(200, 100));
+        addItemButton.setText("Add an item!");
+        addItemButton.addActionListener(this);
+        this.add(addItemButton);
+;    }
+
+    private void initializeFrame() {
+        this.setSize(new Dimension(WIDTH, HEIGHT));
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(BACKGROUND_COLOR);
+        this.setLayout(new FlowLayout());
+        generateTitle();
+        generateBudget();
+    }
+
+    private void generateBudget() {
+        JLabel budget = new JLabel();
+        if (wishlist.getBudget() == 0) {
+            budget.setText("Budget: N/A");
+        } else {
+            budget.setText("Budget: $" + wishlist.getBudget());
+        }
+        budget.setFont(new Font("", Font.ITALIC, 20));
+        this.add(budget);
+    }
+
+    private void generateTitle() {
+        JLabel title = new JLabel(wishlist.getName());
+        title.setIcon(new ImageIcon(ICON_STORE));
+        title.setForeground(TEXT_COLOR);
+        title.setFont(new Font("", Font.BOLD, 30));
+        this.add(title);
+    }
+
+    private void initializeWishlist() {
+        String wishlistName = JOptionPane.showInputDialog("What would you like to call your wishlist?");
+        wishlist = new Wishlist(wishlistName, receiveBudget());
+    }
+
+    private Double receiveBudget() {
+        try {
+            return Double.parseDouble(JOptionPane.showInputDialog("What would you like to set as your budget"));
+        } catch (Exception e) {
+            return receiveBudget();
+        }
+    }
+
+    private void makeItem() {
+        JTextField nameField = new JTextField(10);
+        JTextField quantityField = new JTextField(5);
+        JTextField priceField = new JTextField(5);
+
+        JPanel itemPanel = new JPanel();
+        itemPanel.add(new JLabel("Name:"));
+        itemPanel.add(nameField);
+        itemPanel.add(Box.createHorizontalStrut(12));
+        itemPanel.add(new JLabel("Quantity:"));
+        itemPanel.add(quantityField);
+        itemPanel.add(Box.createHorizontalStrut(12));
+        itemPanel.add(new JLabel("Price: $"));
+        itemPanel.add(priceField);
+
+        int result = JOptionPane.showConfirmDialog(null, itemPanel,
+                "Please enter an item!", JOptionPane.OK_CANCEL_OPTION);
+
+        if (result == 0) {
+            wishlist.addItem(new Item(nameField.getText(), Integer.parseInt(quantityField.getText()),
+                    Double.parseDouble(priceField.getText())));
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addItemButton) {
+            try {
+                makeItem();
+            } catch (Exception ex) {
+                System.out.println("Couldn't add the item due to a caught exception!");
+            }
+        }
     }
 }
